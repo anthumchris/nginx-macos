16d41ad1dd94a7147a491e570a27b5abd5993dcf
Chris <chris@anthum.com>
2023-03-04 00:35:12 -0700

    add build script

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to build Nginx on a Mac
+```sh
+
+```
+
+# nginx-macos-darwin
+Build Nginx for Mac (MacOS) with http2, TLS, and njs (JavaScript)
---
+++ build-nginx.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Exit on errors
+set -e
+
+# INSTALLATION DESTINATION - All files will be installed at the path below and
+# self-contained within that folder, including config, logs, etc.
+#
+#INSTALL_PREFIX=/usr/local/nginx
+INSTALL_PREFIX=$PWD/build/nginx
+
+echo -e "Nginx will build and install at the location below. This should complete in < 5 minutes.\n"
+echo -e "    $INSTALL_PREFIX\n"
+
+# EXIT without confirmation
+while [[ ! $REPLY =~ ^[nNyY]$ ]] ; do read -rp "Start installation? [y/n] "; done
+[[ $REPLY =~ ^[nN]$ ]] && exit 0
+
+# Install required Git submodules
+if [[ ! (-d nginx/.git && -d nginx-njs/.git && -d openssl/.git) ]] ; then
+  echo -e "\nInitializing Git submodules...\n"
+  git submodule update --init
+fi
+
+echo -e "\nConfiguring Nginx...\n"
+cd nginx
+ln -sf auto/configure configure
+./configure \
+  --add-module=../nginx-njs/nginx \
+  --prefix=$INSTALL_PREFIX \
+  --with-cc-opt='-O2 -pipe -fPIE -fPIC -Werror=format-security -D_FORTIFY_SOURCE=2' \
+  --with-compat \
+  --with-http_ssl_module \
+  --with-http_v2_module \
+  --with-openssl=../openssl \
+  --with-threads \
+  ;
+
+echo -e "\nBuilding (a few minutes may pass with no output)...\n"
+make 1>/dev/null --quiet  --jobs=`sysctl -n hw.ncpu` # Faster, parallel build using number of cores
+
+echo -e "\nInstalling..."
+make --quiet install
+
+cd .. 
+
+# Remove newly-created submodule files that Git detects as "Changes not staged for commit"
+# Disable cleanup for repeated builds or changes in Nginx ./configure
+echo -e "Cleaning up...\n"
+git submodule foreach git clean -qfd 1>/dev/null
+
+RELATIVE_PATH=${INSTALL_PREFIX#$PWD/}
+cat <<ENDGINX
+╔═══════════════════════════════════════════════════════════════════════════════
+║
+║   🎉  Nginx was built successfully. Test command:
+║
+║           $RELATIVE_PATH/sbin/nginx -t
+║
+╚═══════════════════════════════════════════════════════════════════════════════
+ENDGINX
